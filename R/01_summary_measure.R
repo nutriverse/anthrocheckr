30238b251b7bc4785fe43cf3f66741089f515859
ernestguevarra <ernestgmd@gmail.com>
2018-03-16 23:06:14 +0000

    update R scripts

--- R/01_summary_measure.R
+++ R/01_summary_measure.R
@@ -20,39 +20,48 @@
 #' # variable is the observer column. This can be used to get the summary
 #' # measure for one measurement type only but can be used to summarise repeat
 #' # measurements
-#'
 #' # Get mean of weight measure taken by each observer at 2 separate occasions
 #'   x <- smartStd[ , c("observer", "weight1")]
 #'   y <- smartStd[ , c("observer", "weight2")]
 #'   names(x) <- names(y) <- c("observer", "weight")
 #'   temp <- data.frame(rbind(x, y))
 #'   mean_measure <- summary_measure(x = temp$weight, index = temp$observer)
+#'   mean_measure
 #'
+#' # Apply summary_measure on smartStdLong dataset that has a row for each subject
+#' # for each measure type and for each repeat measurement. Get mean, sd and max
+#' # of height, weight and MUAC in one specification
+#'   mean_measure <- summary_measure(x = smartStdLong$measure_value,
+#'                                   index = smartStdLong[ , c("observer", "measure_type")])
+#'   mean_measure
 #' @export
-#'
-#'
-#'
-#'
-#'
-#'
 #'
 #
 ################################################################################
 
 summary_measure <- function(x, index) {
-
+  #
+  # Get mean of measurements
+  #
   sm_mean <- tapply(X = x, INDEX = index, FUN = mean, na.rm = TRUE)
-
+  #
+  # Get SD of measurements
+  #
   sm_sd <- tapply(X = x, INDEX = index, FUN = sd, na.rm = FALSE)
-
+  #
+  # Get max value of measurements
+  #
   sm_max <- tapply(X = x, INDEX = index, FUN = max, na.rm = FALSE)
-
-  if(is.list(index) | is.data.frame(index)) {
-    sm <- as.list(sm_mean, sm_sd, sm_max)
+  #
+  # If index is a list...
+  #
+  if(is.list(index)) {
+    sm <- list(data.frame(sm_mean), data.frame(sm_sd), data.frame(sm_max))
+    names(sm) <- c("mean", "sd", "max")
+  } else {
+    sm <- data.frame(sm_mean, sm_sd, sm_max)
     names(sm) <- c("mean", "sd", "max")
   }
-  sm <- data.frame(sm_mean, sm_sd, sm_max)
-  names(sm) <- c("mean", "sd", "max")
   #
   # Return output
   #
--- R/05_calculate_relative_tem.R
+++ R/05_calculate_relative_tem.R
@@ -1,29 +1,51 @@
 ################################################################################
 #
+#' calculate_relative_tem
 #'
+#' Calculate relative TEM of measurements by multiple observers.
 #'
+#' @param tem A numeric vector or data frame of technical error of measurements
+#'     produced from applying \code{calculate_tem_cohort()}.
+#' @param mean_value A numeric vector or data frame of mean of measurements
+#'     produced from applying \code{summary_measure()}.
 #'
+#' @return A vector or data frame of calculated relative TEM per observer and per
+#'     measurement type.
 #'
+#' @examples
+#' # Calculate relative TEM for weight using the smartStd dataset
+#'   tem <- calculate_tem_cohort(m1 = smartStd$muac1, m2 = smartStd$muac2,
+#'     index = smartStd$observer, n = 10)
+#'   mean_value <- summary_measure(x = c(smartStd$weight1, smartStd$weight2),
+#'     index = rep(smartStd$observer, 2))$mean
+#'   rel_tem <- calculate_relative_tem(tem = tem, mean_value = mean_value)
+#'   rel_tem
 #'
+#' # Calculate relative TEM using smartStdLong dataset
+#'   tem <- calculate_tem_cohort(m1 = smartStdLong$measure_value[smartStdLong$measure_round == 1],
+#'     m2 = smartStdLong$measure_value[smartStdLong$measure_round == 2],
+#'     index = smartStdLong[smartStdLong$measure_round == 1, c("observer", "measure_type")],
+#'     n = 10)
+#'   mean_value <- summary_measure(x = smartStdLong$measure_value,
+#'     index = smartStdLong[ , c("observer", "measure_type")])$mean
+#'   rel_tem <- calculate_relative_tem(tem = tem, mean_value = mean_value)
+#'   rel_tem
 #'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
+#' @export
 #'
 #
 ################################################################################
 
-
+calculate_relative_tem <- function(tem, mean_value) {
+  #
+  # Calculate relative TEM
+  #
+  relative_tem <- tem / mean_value * 100
+  #
+  # Return output
+  #
+  return(relative_tem)
+}
 
 
 
