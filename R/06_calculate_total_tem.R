dbc91b39d75579aad79cadaeef1682729dfff664
ernestguevarra <ernestgmd@gmail.com>
2018-03-17 08:30:51 +0000

    update R scripts

--- R/06_calculate_total_tem.R
+++ R/06_calculate_total_tem.R
@@ -13,9 +13,18 @@
 #' # Calculate total
 #' intra <- calculate_tem_cohort(m1 = smartStd$muac1, m2 = smartStd$muac2,
 #'   index = smartStd$observer, n = 11)
-#' inter <- calculate_team_tem(n = 10, k = 11, m = stature[ , c("m1", "m2", "m3", "m4")])
+#' # Get inter
+#' muacDF1 <- subset(smartStd, select = c(-muac2, -weight1, -weight2, -height1, -height2))
+#' muacDF2 <- subset(smartStd, select = c(-muac1, -weight1, -weight2, -height1, -height2))
 #'
+#' # Spread the dataset to wide format
+#' #library(tidyr)
+#' muacDF1 <- tidyr::spread(muacDF1, key = "observer", value = "muac1")
+#' muacDF2 <- tidyr::spread(muacDF2, key = "observer", value = "muac2")
+#' muacDF <- merge(muacDF1, muacDF2, by = "subject")
+#' inter <- calculate_team_tem(n = 10, k = 22, m = muacDF[ , 2:23])
 #'
+#' total_tem <- calculate_total_tem(intra = intra, inter = inter)
 #'
 #' @export
 #'
---
+++ R/07_calculate_coeff_r.R
@@ -0,0 +1,26 @@
+################################################################################
+#
+#' calculate_coeff_r
+#'
+#' Calculate the coefficient of reliability (R).
+#'
+#' @param total_tem A numeric value for total TEM
+#' @param sd A numeric vector for standard deviation of measurements
+#'
+#' @return A numeric vector of coefficient of reliability values per observer
+#'
+#' @examples
+#' #
+#'
+#'
+#' @export
+#'
+#
+################################################################################
+
+calculate_coeff_r <- function(total_tem, sd) {
+
+  coeff_r <- (total_tem ^ 2) / sd
+
+  return(coeff_r)
+}
