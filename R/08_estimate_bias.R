689b5e3434e126a9e92b54907c2a8948c30ede38
ernestguevarra <ernestgmd@gmail.com>
2018-03-18 05:09:06 +0000

    add estimate_bias.R script

---
+++ R/08_estimate_bias.R
@@ -0,0 +1,41 @@
+################################################################################
+#
+#' estimate_bias
+#'
+#' Calculate the bias of a measurement against a gold standard. Two gold standards
+#' are used in this function: 1) measurements made by a supervisor or a known
+#' expert; and, 2) median of all measurements made by the observers.
+#'
+#' @param msur A numeric value or vector of mean measurements from observers
+#' @param msup A numeric value or vector of mean measurements from supervisor
+#' @param mall A numeric value or vector of the median of  measurements from
+#'     observers
+#'
+#' @return A numeric value or vector signifying bias
+#'
+#' @examples
+#' #
+#'
+#' @export
+#'
+#
+################################################################################
+
+estimate_bias <- function(msur, msup, mall) {
+  #
+  # Bias from supervisor
+  #
+  bias_super <- msur - msup
+  #
+  # Bias from median
+  #
+  bias_med <- msur - mall
+  #
+  # Concatenate into a data frame
+  #
+  bias <- data.frame(bias_super, bias_med)
+  #
+  # Return output
+  #
+  return(bias)
+}
