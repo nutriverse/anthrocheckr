15065f6054478053a24d396e4162fa779a8c2509
Ernest Guevarra <ernest@validmeasures.org>
2018-12-23 17:15:26 +0000

    add figures and new package badge

---
+++ inst/createHexSticker.R
@@ -0,0 +1,7 @@
+library(hexSticker)
+
+sticker("inst/figures/anthrocheckr.png",
+        s_x = 1, s_y = 0.8, s_width = 0.6,
+        package = "anthrocheckr", p_color = "#797842", p_y = 1.4, p_size = 6,
+        h_size = 1, h_color = "#797842", h_fill = NA,
+        filename = "man/figures/anthrocheckr.png")
