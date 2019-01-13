3f26b8cd8f0acc693f03131d7b51bc1760fbbf66
Ernest Guevarra <ernest@validmeasures.org>
2019-01-13 06:40:29 +0000

    update createHexSticker.R

--- inst/createHexSticker.R
+++ inst/createHexSticker.R
@@ -1,7 +1,7 @@
 library(hexSticker)
 
 sticker("inst/figures/anthrocheckr.png",
-        s_x = 1, s_y = 1, s_width = 0.75,
-        package = "anthrocheckr", p_color = "#797842", p_y = 1.4, p_size = 6,
-        h_size = 1, h_color = "#797842", h_fill = NA,
+        s_x = 1, s_y = 0.85, s_width = 0.60,
+        package = "anthrocheckr", p_color = "#797842", p_y = 1.5, p_size = 5,
+        h_size = 1.5, h_color = "#797842", h_fill = NA,
         filename = "man/figures/anthrocheckr.png")
