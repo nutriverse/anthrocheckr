1fdb5bfe996536aeb0877ff8a778a17263a5b474
Ernest Guevarra <ernest@validmeasures.org>
2019-04-25 06:37:50 +0100

    update createHexSticker.R

--- inst/createHexSticker.R
+++ inst/createHexSticker.R
@@ -1,7 +1,19 @@
 library(hexSticker)
 
-sticker("inst/figures/anthrocheckr.png",
-        s_x = 1, s_y = 0.85, s_width = 0.60,
-        package = "anthrocheckr", p_color = "#797842", p_y = 1.5, p_size = 5,
-        h_size = 1.5, h_color = "#797842", h_fill = NA,
-        filename = "man/figures/anthrocheckr.png")
+sticker("inst/figures/davinci.png",
+        s_x = 1, s_y = 1.1, s_width = 0.70,
+        package = "anthrocheckr", p_color = "#250042", p_y = 1, p_size = 6,
+        h_size = 1.5, h_color = "#004225", h_fill = "#007542",
+        filename = "man/figures/anthrocheckr.png", dpi = 600)
+
+sticker("inst/figures/davinci.png",
+        s_x = 1, s_y = 1.1, s_width = 0.70,
+        package = "anthrocheckr", p_color = "#004225", p_y = 1, p_size = 6,
+        h_size = 1.5, h_color = "#004225", h_fill = NA,
+        filename = "man/figures/anthrocheckr.png", dpi = 600)
+
+sticker("inst/figures/weigh_scale_2.png",
+        s_x = 1, s_y = 1.1, s_width = 0.60,
+        package = "anthrocheckr", p_color = "#250042", p_y = 1, p_size = 6,
+        h_size = 1.5, h_color = "#004225", h_fill = "#007542",
+        filename = "man/figures/anthrocheckr.png", dpi = 600)
