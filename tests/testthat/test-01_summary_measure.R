e220b83a48f40a1ff7f819798a1f1439fc13018a
Ernest Guevarra <ernest@guevarra.io>
2020-12-05 22:52:11 +0000

    update tests

--- tests/testthat/test-01_summary_measure.R
+++ tests/testthat/test-01_summary_measure.R
@@ -1,28 +1,30 @@
 library(dplyr)
 
 ## Summary measure for each observer for each anthropometric measurement
-x <- summary_measure(x = smartStdLong$measure_value,
-                     index = smartStdLong[ , c("observer", "measure_type")])
+x <- summary_measure(df = smartStdLong,
+                     measures = "measure_value",
+                     index = c("observer", "measure_type"))
 
-test_that("output is a list", {
-  expect_is(x, "list")
+test_that("output is a data.frame", {
+  expect_is(x, "data.frame")
 })
 
 test_that("names of output is correct", {
-  expect_true(all(names(x) %in% c("mean", "sd", "max")))
+  expect_true(all(names(x) %in% c("observer", "measure_type",
+                                  "mean", "sd", "max")))
 })
 
 ## Summary measure for one observer for each anthropometric measurement
 x <- smartStdLong %>%
   filter(observer == 1)
 
-y <- summary_measure(x = x$measure_value, index = x$measure_type)
+y <- summary_measure(df = x, measures = "measure_value", index = "measure_type")
 
 test_that("output is a data.frame", {
   expect_is(y, "data.frame")
 })
 
 test_that("names of output is correct", {
-  expect_true(all(names(y) %in% c("mean", "sd", "max")))
+  expect_true(all(names(y) %in% c("measure_type", "mean", "sd", "max")))
 })
 
